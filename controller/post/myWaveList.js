a15d14e75139575a9b51764700f619a58824f8b3
doumKim <eha0129@naver.com>
2020-10-03 16:37:26 +0900

    [Server] /#43/ Fix: mywavelist module path
    myWaveList에서 mapToPostsLikeValue 모듈 경로 수정

--- controller/post/myWaveList.js
+++ controller/post/myWaveList.js
@@ -1,10 +1,12 @@
 const { Post } = require("../../models");
+const { mapToPostsLikeValue } = require("../helper");
 module.exports = {
   //내가 일으킨 파도 목록(내가 작성한 글 목록)
   get: async (req, res) => {
     try {
       const count = req.query.count;
       const sort = req.query.sort; // create_at, like (recent,hot)
+      const passport = req.query.passport;
       const myWaveList = await Post.findAll({
         offset: 0,
         limit: Number(count) || 100,
@@ -13,6 +15,8 @@ module.exports = {
           create_user: req.session.passport.user,
         },
       });
+
+      await mapToPostsLikeValue(myWaveList, passport);
       res.status(200).json(myWaveList);
     } catch (err) {
       res.status(401).send("Invalid Access");
