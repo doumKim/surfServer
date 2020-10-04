f29b3262ac76ec39ef288995d61376709612de6c
doumKim <eha0129@naver.com>
2020-10-04 20:46:16 +0900

    [Server] /#56/ Feature: inifinite scroll
    메인 인피니트 스크롤 처리를 위한 allWave offset 쿼리 추가 및 limit 10 설정

--- controller/post/allWave.js
+++ controller/post/allWave.js
@@ -3,15 +3,17 @@ const { Post, User } = require("../../models");
 const sequelize = require("sequelize");
 const { mapToPostsLikeValue } = require("../helper");
 const Op = sequelize.Op;
-
 module.exports = {
   get: async (req, res) => {
     try {
       const category = req.query.category; // 카테고리(전체,액션,로맨스,판타지,스릴러,...)
       const sort = req.query.sort; // 인기,최신 (like, created_at)
+      const offset = req.query.offset || 0;
       const passport = req.session.passport;
       if (category) {
         const posts = await Post.findAll({
+          offset: Number(offset) || 0,
+          limit: 10,
           where: {
             categories: {
               [Op.like]: "%" + category + "%",
@@ -24,11 +26,12 @@ module.exports = {
             attributes: ["avartar_url", "username"],
           },
         });
-
         await mapToPostsLikeValue(posts, passport);
         res.status(200).send(posts);
       } else {
         const posts = await Post.findAll({
+          offset: Number(offset) || 0,
+          limit: 10,
           order: [[sort, "DESC"]],
           include: [
             {
@@ -38,7 +41,6 @@ module.exports = {
             },
           ],
         });
-
         await mapToPostsLikeValue(posts, passport);
         res.status(200).send(posts);
       }
